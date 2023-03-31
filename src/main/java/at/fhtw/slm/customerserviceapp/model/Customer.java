dd43479e2790e3ba62887dca3854103b8892de21
wallisch <walliscb@technikum-wien.at>
2023-03-31 13:57:54 +0200

    add Customer class

---
+++ src/main/java/at/fhtw/slm/customerserviceapp/model/Customer.java
@@ -0,0 +1,16 @@
+package at.fhtw.slm.customerserviceapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Customer {
+    private String name;
+    private String lastname;
+    private LocalDate dayOfBirth;
+}
